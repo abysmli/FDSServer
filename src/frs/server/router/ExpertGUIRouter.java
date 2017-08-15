3c150c3f6c419c557a548085df3d1f9d6c9fb3ee
abysmli <abysmli@gmail.com>
2017-08-15 13:37:52 +0200

    add expert

--- src/frs/server/router/ExpertGUIRouter.java
+++ src/frs/server/router/ExpertGUIRouter.java
@@ -5,10 +5,34 @@
  */
 package frs.server.router;
 
+import frs.server.model.FaultDatabaseHandler;
+import frs.server.model.SymptomDatabaseHandler;
+import frs.server.model.SystemDatabaseHandler;
+import java.sql.SQLException;
+import javax.naming.NamingException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
+
 /**
  *
  * @author abysmli
  */
+@Path("/expert")
 public class ExpertGUIRouter {
-    
+
+    private final SymptomDatabaseHandler databaseSymptom = new SymptomDatabaseHandler();
+    private final FaultDatabaseHandler databaseFault = new FaultDatabaseHandler();
+    private final SystemDatabaseHandler databaseSystem = new SystemDatabaseHandler();
+
+    @Path("/getFaults")
+    @GET
+    @Produces("application/json;charset=utf-8")
+    public Response getFaults() throws JSONException, NamingException, SQLException {
+        JSONArray faults = databaseFault.getFaultKnowledge();
+        return Response.status(200).entity(faults.toString()).build();
+    }
 }
